e828ce58f4166aa403debe64623b9bab08270300
herciunichita <nherciu@cloudbasesolutions.com>
2016-05-18 22:58:24 +0300

    added fps controller and entity with basic chasing AI

---
+++ end/Assets/scripts/entities/BasicFollowerAI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicFollowerAI : MonoBehaviour {
+    public Transform target;
+    public Transform follower;
+    public float speed = 10;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(target);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+	}
+}
