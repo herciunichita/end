c028a942ab172c16ad774c7cf27cb865630396d4
herciunichita <nherciu@cloudbasesolutions.com>
2016-06-03 17:55:19 +0300

    added timer, random teleports to follower

--- end/Assets/scripts/StartupScript.cs
+++ end/Assets/scripts/StartupScript.cs
@@ -2,12 +2,36 @@
 using System.Collections;
 
 public class StartupScript : MonoBehaviour {
-    
+    public float timer;
+    private float internaltimer;
     void Awake()
     {
         //MapDisplay display = FindObjectOfType<MapDisplay>();
         MapGenerator mapgen = FindObjectOfType<MapGenerator>();
         mapgen.generateMap();
+        internaltimer = timer;
+    }
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            timer = 0;
+        }
+
+    }
+
+    void OnGUI()
+    {
+        
+        if (timer == 0)
+        {
+            GUI.Box(new Rect(10, 10, 300, 30), "Congratulations, you have survived the end.");
+        }
+        else
+        {
+            GUI.Box(new Rect(10, 10, 60, 20), "" + ((int)timer).ToString());
+        }
     }
 
 }
--- end/Assets/scripts/entities/BasicFollowerAI.cs
+++ end/Assets/scripts/entities/BasicFollowerAI.cs
@@ -15,16 +15,43 @@ public class BasicFollowerAI : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Vector3.Distance(follower.position, target.position) > 50 )
+        if (Vector3.Distance(follower.position, target.position) > 100 )
         {
             actualspeed = 80f;
+            transform.LookAt(target);
+            transform.Translate(Vector3.forward * actualspeed * Time.deltaTime);
+            transform.Translate(Vector3.up * hover * Time.deltaTime);
+        }
+        else if (Vector3.Distance(follower.position, target.position) > 20)
+        {
+            System.Random prng = new System.Random();
+            actualspeed = speed;
+            transform.LookAt(target);
+            int decision = prng.Next(1, 500);
+            if (decision >= 499)
+            {
+                actualspeed = 0f;
+                if ((int)prng.Next(1, 500) % 2 == 0)
+                {
+                    transform.Translate(Vector3.left * 2500f * Time.deltaTime);
+                    transform.Translate(Vector3.forward * 2000f * Time.deltaTime);
+                }
+                else
+                {
+                    transform.Translate(Vector3.right * 2500f * 2 * Time.deltaTime);
+                    transform.Translate(Vector3.forward * 2000f * Time.deltaTime);
+                }
+
+            }
+            transform.Translate(Vector3.forward * actualspeed * Time.deltaTime);
+            transform.Translate(Vector3.up * hover * Time.deltaTime);
         }
         else
         {
-            actualspeed = speed;
+            transform.LookAt(target);
+            transform.Translate(Vector3.forward * actualspeed * Time.deltaTime);
+            transform.Translate(Vector3.up * hover * Time.deltaTime);
         }
-        transform.LookAt(target);
-        transform.Translate(Vector3.forward * actualspeed * Time.deltaTime);
-        transform.Translate(Vector3.up * hover * Time.deltaTime);
+
 	}
 }
